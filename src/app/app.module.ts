cd2ad80f415a70e20a76050c6607c7be3b1a0980
Jhon Ureta <jhonureta75@gmail.com>
2021-07-12 18:04:01 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroe.module';
+import { ContadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
+
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
